aa4d024acf5314414ae3437e44296576dc1b54aa
Kamil Kaszyca <kkk@agh.edu.pl>
2021-12-14 21:12:35 +0100

    update testing

--- R/finterpolate.test.R
+++ R/finterpolate.test.R
@@ -12,9 +12,15 @@ x_vec = seq(from = out.start, to = out.end, by = out.by)
 y_lin = interpolate(x_in = xT, y_in = yT, x_out = x_vec, method = "linear")
 y_spl = interpolate(x_in = xT, y_in = yT, x_out = x_vec, method = "spline")
 
+
+png(filename = "plot/G01-Lin-vs-Spl.png")
 plot (x = xT, y = yT, type = 'p', 
       xlim = c(out.start-1, out.end+1), ylim = c(0,15),
-      ylab = "linear",
+      ylab = "fit", xlab = "Independent variable",
       lwd = 2)
 lines(x = x_vec, y=y_lin, col = "blue", lwd = 2)
 lines(x = x_vec, y=y_spl, col = "red", lwd = 2)
+legend(-2, 15, legend=c("raw", "Splined", "Linear"),
+       col=c("black","red", "blue"), lty=c(1,1,1), cex=0.8)
+dev.off()
+
