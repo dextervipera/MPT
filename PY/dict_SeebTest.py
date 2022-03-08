1afb3073c66f664a8937e422376fac9327edc658
KKK <dextervipera@gmail.com>
2022-03-08 18:31:19 +0100

    data treatement develop[ement

---
+++ PY/dict_SeebTest.py
@@ -0,0 +1,7 @@
+SeebTest01 = dict(0:"LP", "temperature":1, "temperatureABS":2, "tempInv":3, "seebeck":4, "seebeckERR":5, "resistivity":6,
+                  "resistivityERR":7, "swl":8, "sweERR":9, "log_swl":10, "tempLdt0", "tempHdt0", "tempLdtX", "tempHdtX", "dT",
+                  "SeebAvgUdt0", "SeebAvgUdt0ERR", "SeebAvgUdtX", "SeebAvgUdtXERR", "swlAVGU", "swlAvgUERR", "swlAvgI",
+                  "swlAvgIERR", "PowF", "Lambda", "ZT")
+%:,XX
+testdict = dict(0:"Lambda", 1:"L.P.")
+
--- PY/finterpolate.py
+++ PY/finterpolate.py
@@ -1,5 +1,18 @@
 import numpy as np
-import  pandas as pd
+import pandas as pd
+from scipy.interpolate import interp1d
 
-electrical = pd.read_csv("example\MPT-electrical.txt", delimiter="\t", header=[0])
-print (electrical)
+def seebtest_v01_interp(heat, electrical):
+    heat_interp = interp1d(heat.loc[:,"Temperature"], heat.loc[:,"Lambda"], bounds_error=False, fill_value="extrapolate")
+    for i in range(1,len(electrical)):
+         electrical.loc[i,"Lambda"] = heat_interp(electrical.loc[i,"Temperatura"])
+         print(i, electrical.loc[i,"Lambda"])
+
+def seebtest_v01_recalczt(electrical):
+    for i in range(1, len(electrical)):
+        electrical.iloc[i, 18] = electrical.loc[i, 3]
+
+if __name__ == "__main__":
+    pass
+
+
