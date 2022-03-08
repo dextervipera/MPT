50bf77a23008f2d3bb58a9de6a0ab084c06d8b33
dextervipera <89567341+dextervipera@users.noreply.github.com>
2022-03-08 20:43:45 +0100

    R developement changes

--- R/finterpolate.test.R
+++ R/finterpolate.test.R
@@ -6,7 +6,7 @@ xT = 1:10
 yT = (1:10)^2/10
 out.start = -1;
 out.end = 12;
-out.by = 0.5;
+out.by = 0.25;
 
 x_vec = seq(from = out.start, to = out.end, by = out.by)
 y_con = interpolate(x_in = xT, y_in = yT, x_out = x_vec, method = "constant")
@@ -18,10 +18,10 @@ png(filename = "plot/G01-Lin-vs-Spl.png")
 plot (x = xT, y = yT, type = 'p', 
       xlim = c(out.start-1, out.end+1), ylim = c(0,15),
       ylab = "fit", xlab = "Independent variable",
-      lwd = 2)
-lines(x = x_vec, y=y_con, col = "green", lwd = 2)
-lines(x = x_vec, y=y_lin, col = "blue", lwd = 2)
-lines(x = x_vec, y=y_spl, col = "red", lwd = 2)
+      lwd = 2, pch = 3, cex =2)
+points(x = x_vec, y=y_con, col = "green", lwd = 2)
+points(x = x_vec, y=y_lin, col = "blue", lwd = 2)
+points(x = x_vec, y=y_spl, col = "red", lwd = 2)
 grid()
 legend(-2, 15, legend=c("Raw", "Constant", "Linear", "Splined"),
        col=c("black","green", "blue", "red"), lty=c(1,1,1,1), cex=0.8)
