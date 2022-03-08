d479b0c2d5be67e264e8ad10d55de9ea83d46be7
dextervipera <89567341+dextervipera@users.noreply.github.com>
2022-03-08 23:47:28 +0100

    basic recalc function add header err fix when 2-row is used

--- PY/dict_SeebTest.py
+++ PY/dict_SeebTest.py
@@ -1,4 +1,12 @@
-dictST = dict(LP=0, temperature=1, temperatureABS=2, tempInv=3, seebeck=4, seebeckERR=5, resistivity=6, resistivityERR=7, swl=8, sweERR=9,
-              log_swl=10, tempLdt0=11, tempHdt0=12, tempLdtX=13, tempHdtX=14, dT=15, SeebAvgUdt0=16, SeebAvgUdt0ERR=17, SeebAvgUdtX=18,
-              SeebAvgUdtXERR=19, swlAVGU=20, swlAvgUERR=21, swlAvgI=22, swlAvgIERR=23, PowF=24, Lambda=25, ZT=26)
+dictST = {"LP": 0,
+          "temperature": 1,
+          "temperatureABS": 2,
+          "tempInv": 3,
+          "seebeck": 4, "seebeckERR": 5,
+          "resistivity": 6, "resistivityERR": 7,
+          "swl": 8, "sweERR": 9, "log_swl": 10,
+          "tempLdt0": 11, "tempHdt0": 12, "tempLdtX": 13, "tempHdtX": 14, "dT": 15,
+          "SeebAvgUdt0": 16, "SeebAvgUdt0ERR": 17, "SeebAvgUdtX": 18, "SeebAvgUdtXERR": 19,
+          "swlAVGU": 20, "swlAvgUERR": 21, "swlAvgI": 22, "swlAvgIERR": 23,
+          "PowF": 24, "Lambda": 25, "ZT": 26}
 
